972067e866edac9bbf573e72c5a54960038cd568
davilla20 <davilla20@yahoo.com>
2019-10-18 12:36:34 +0300

    Replace double quote with single quote

--- app/Http/Controllers/ReportIssuesController.php
+++ app/Http/Controllers/ReportIssuesController.php
@@ -13,7 +13,7 @@ class ReportIssuesController extends Controller
 {
     public function create() 
     {
-        return Inertia::render("Static/Allegation", [
+        return Inertia::render('Static/Allegation', [
             "types" => AllegationType::with("categories")->get(),
             "regions" => Region::with("districts")->get(),
             "relationships" => SuspectRelationship::get()
